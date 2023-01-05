e0aa694208322a59952668f9a74fd2184689ef7a
Luke <87561450+OperationNorthwoods@users.noreply.github.com>
2023-01-05 15:27:44 -0500

    more learning (ch6)
    added book code for piglatin. added 2 more files for ch6 proj, haven't started yet.

--- ch6PigLatin.py
+++ ch6PigLatin.py
@@ -1,1 +1,53 @@
-# skipping for now. will come back and do later.
+#english to pig latin
+message = input("Enter the English message to translater into Pig Latin: ")
+
+vowels = ('a', 'e', 'i', 'o', 'u', 'y')
+
+pigLatin = [] # a list of words in pig latin
+for word in message.split():
+    prefixNonLetters = ''
+    while len(word) > 0 and not word[0].isalpha():
+        prefixNonLetters += word[0]
+        word = word[1:]
+    if len(word) == 0:
+        pigLatin.append(prefixNonLetters)
+        continue
+
+    # Seperate the non-letters at the end of this word:
+    suffixNonLetters = ''
+    while not word[-1].isalpha():
+        suffixNonLetters += word[-1]
+        word = word[:-1]
+    
+    # Remember if the word was in uppercase or title case.
+    wasUpper = word.isupper()
+    wasTitle = word.istitle()
+
+    word = word.lower() # Make the word lowercase for translation
+
+    # Seperate the consonants at the start of this word:
+    prefixConsonants = ''
+    while len(word) > 0 and not word[0] in VOWELS:
+        prefixConsonants += word[0]
+        word = word[1:]
+    
+    # Add the Pig Latin ending to the word:
+    if prefixConsonants != '':
+        word += prefixConsonants + 'ay'
+    else:
+        word += 'yay'
+
+    # Set the word back to uppercase or title case:
+    if wasUpper:
+        word = word.upper()
+    if wasTitle:
+        word = word.title()
+
+    #add the non-letters back to the start or the end of the word.
+    pigLatin.append(prefixNonLetters + word + suffixNonLetters)
+
+# Join all the words back together into a single string:
+print(' '.join(pigLatin))
+
+
+#typed out from book
